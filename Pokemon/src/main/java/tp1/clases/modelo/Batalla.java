ea7ff8ddaed43ca6f93b6a4baf0b63654c1fa5b5
melina-r <mretamozo@fi.uba.ar>
2023-09-26 14:41:38 -0300

    Añado métodos a Batalla

--- Pokemon/src/main/java/tp1/clases/modelo/Batalla.java
+++ Pokemon/src/main/java/tp1/clases/modelo/Batalla.java
@@ -1,24 +1,31 @@
 package tp1.clases.modelo;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class Batalla {
-    private ArrayList<Jugador> jugadores;
+    private final ArrayList<Jugador> jugadores;
+    private ArrayList<Jugador> jugadoresEnJuego;
     private int turno;
 
     public Batalla(ArrayList<Jugador> jugadores) {
         this.jugadores = jugadores;
+        this.jugadoresEnJuego = jugadores;
         determinarJugadorInicial();
+    }
+
+    public ArrayList<Jugador> getJugadores() {
+        return jugadores;
     }
 
     private void determinarJugadorInicial() {
         int primero = 0;
         int mayorVelocidad = 0;
-        for (int i = 0; i < this.jugadores.size(); i++) {
-         if (this.jugadores.get(i).getPokemonActual().getVelocidad() > mayorVelocidad) {
+        for (int i = 0; i < this.jugadoresEnJuego.size(); i++) {
+         if (this.jugadoresEnJuego.get(i).getPokemonActual().getVelocidad() > mayorVelocidad) {
              primero = i;
-             mayorVelocidad = this.jugadores.get(i).getPokemonActual().getVelocidad();
+             mayorVelocidad = this.jugadoresEnJuego.get(i).getPokemonActual().getVelocidad();
          }
         }
 
@@ -26,12 +33,17 @@ public class Batalla {
     }
 
     public Optional<Jugador> obtenerGanador() {
-       ArrayList<Jugador> jugadoresConVida = new ArrayList<>();
-        for (Jugador jugador : this.jugadores) {
+        if (this.jugadoresEnJuego.size() == 1) {
+            return Optional.of(this.jugadoresEnJuego.get(0));
+        }
+
+        ArrayList<Jugador> jugadoresConVida = new ArrayList<>();
+        for (Jugador jugador : this.jugadoresEnJuego) {
             if (jugador.tienePokemonesConVida()) {
                 jugadoresConVida.add(jugador);
             }
         }
+
         if (jugadoresConVida.size() ==  1) {
             return Optional.of(jugadoresConVida.get(0));
         } else {
@@ -43,9 +55,26 @@ public class Batalla {
         this.turno += 1;
     }
 
-    public Jugador getTurno() {
+    public Jugador getJugadorActual() {
         return this.jugadores.get(this.turno % this.jugadores.size());
     }
 
+    public void rendir(Jugador jugador) {
+        this.jugadoresEnJuego.remove(jugador);
+    }
 
+    public List<Habilidad> getHabilidadesPokemonActual() {
+        return this.getJugadorActual().getPokemonActual().getHabilidades();
+    }
+    public void usarAtaque(Habilidad habilidad, Jugador jugadorRival) {
+        habilidad.usar(this.getJugadorActual().getPokemonActual(), jugadorRival.getPokemonActual());
+    }
+
+    public void usarItem(Item item) {
+        item.usar(this.getJugadorActual().getPokemonActual());
+    }
+
+    public void cambiarPokemon(Pokemon pokemon) {
+        this.getJugadorActual().seleccionarPokemon(pokemon); //creo que seleccionar pokemon debería recibir el pokemon en lugar del índice en la lista
+    }
 }
