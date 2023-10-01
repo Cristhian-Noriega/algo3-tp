71381977cb24510e772a80477b1aaa8eaf4da76d
melina-r <mretamozo@fi.uba.ar>
2023-09-30 22:06:33 -0300

    arreglo errores en batalla

--- Pokemon/src/main/java/tp1/clases/modelo/Batalla.java
+++ Pokemon/src/main/java/tp1/clases/modelo/Batalla.java
@@ -20,10 +20,10 @@ public class Batalla {
     private void determinarJugadorInicial() {
         int primero = 0;
         int mayorVelocidad = 0;
-        for (int i = 0; i < this.jugadoresEnJuego.size(); i++) {
-           if (this.jugadoresEnJuego.get(i).getVelocidadPokemonActual() > mayorVelocidad) {
+        for (int i = 0; i < this.jugadores.size(); i++) {
+           if (this.jugadores.get(i).getVelocidadPokemonActual() > mayorVelocidad) {
                primero = i;
-               mayorVelocidad = this.jugadoresEnJuego.get(i).getVelocidadPokemonActual();
+               mayorVelocidad = this.jugadores.get(i).getVelocidadPokemonActual();
            }
         }
 
@@ -52,7 +52,7 @@ public class Batalla {
     }
 
     public List<Habilidad> getHabilidadesPokemonActual() {
-        return this.getJugadorActual().getHabilidadesPokemonActual;
+        return this.getJugadorActual().getHabilidadesPokemonActual();
     }
     public void usarAtaque(Habilidad habilidad, Jugador jugadorRival) {
         habilidad.usar( this.getJugadorActual().getPokemonActual(), jugadorRival.getPokemonActual());
