ecd6eb6762942cd56868632a279566ac7ea84933
emiliaduzac <eduzac@fi.uba.ar>
2023-10-01 18:44:20 -0300

    Creo inicializador y le doy forma al main

--- Pokemon/src/main/java/tp1/clases/Main.java
+++ Pokemon/src/main/java/tp1/clases/Main.java
@@ -1,28 +1,33 @@
 package tp1.clases;
 
 import tp1.clases.modelo.Batalla;
-import tp1.clases.modelo.Inicializador;
+import tp1.clases.modelo.Constantes;
+import tp1.clases.modelo.Jugador;
 import tp1.clases.vista.ResultadoView;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class Main {
+    public static void main(String[] args) throws IOException{
+        try{
+            Inicializador inicializador = new Inicializador();
+            Batalla batalla = new Batalla(inicializador.iniciarJugadores(Constantes.cantidadJugadores));
+            Controlador controlador = new Controlador(batalla);
 
-    Inicializador inicializador = new Inicializador();
+            boolean juegoTerminado = false;
+            while (!juegoTerminado){
+                controlador.Jugar();
+                juegoTerminado = controlador.getJuegoTerminado();
+            }
 
-    Batalla batalla = new Batalla(inicializador.getJugadores());
-
-    Controlador controlador = new Controlador(batalla);
-
-    while!controlador.getJuegoTerminado()
-
-    {
-        controlador.Jugar();
+            // batalla.ObtenerGanador() esta devolviendo un Jugador y resultadoView necesita el nombre. Para que resultadoView
+            // se abstraiga de obtener el nombre, que batalla devuelva directo el String del nombre
+            Optional<Jugador> res = ResultadoView.mostrarResultado(batalla.obtenerGanador());
+            
+        } catch (IOException err){
+            System.out.println("Hubo un error al inicializar el juego");
+            // ??
+        }
     }
-    String res = ResultadoView.mostrarResultado(batalla.obtenerGanador());
-
-    public Main() throws IOException { //como trabajar el IOException
-    }
-
-    System.out.println(res);
 }
