32c7eaa3c7a41ad70ba12cb7e8f2b603d0b1d63c
melina-r <mretamozo@fi.uba.ar>
2023-10-24 15:33:51 -0300

    saco los conflictos del codigo

--- Pokemon/src/main/java/tp1/clases/modelo/Clima.java
+++ Pokemon/src/main/java/tp1/clases/modelo/Clima.java
@@ -8,12 +8,8 @@ public enum Clima {
     TORMENTA_DE_ARENA(List.of(Tipo.TIERRA, Tipo.ROCA)),
     NIEBLA(List.of(Tipo.FANTASMA, Tipo.PSIQUICO)),
     TORMENTA_DE_RAYOS(List.of(Tipo.ELECTRICO)),
-<<<<<<< HEAD
     HURACAN(List.of(Tipo.VOLADOR)),
     SIN_CLIMA(null);
-=======
-    HURACAN(List.of(Tipo.VOLADOR));
->>>>>>> 4f5c1277241255de37e7d486195a94fc4e99bf77
 
     private final List<Tipo> tiposFavorecidos;
 
@@ -24,24 +20,14 @@ public enum Clima {
     public List<Tipo> getTiposFavorecidos() {
         return tiposFavorecidos;
     }
-
-<<<<<<< HEAD
     public double mejorarAtaque(Tipo tipo, double danio) {
         if (this.tiposFavorecidos.contains(tipo)) {
-=======
-    public double mejorarAtaque(Pokemon pokemon, double danio) {
-        if (this.tiposFavorecidos.contains(pokemon.getTipo())) {
->>>>>>> 4f5c1277241255de37e7d486195a94fc4e99bf77
             return (danio * Constantes.modificacionPorClima) + danio;
         }
         return danio;
     }
 
-<<<<<<< HEAD
     public void lastimarPorClima(Pokemon pokemon) {
-=======
-    public void lastimar(Pokemon pokemon) {
->>>>>>> 4f5c1277241255de37e7d486195a94fc4e99bf77
         if (this == Clima.HURACAN || this == Clima.TORMENTA_DE_ARENA || this == Clima.TORMENTA_DE_RAYOS) {
             if (!this.getTiposFavorecidos().contains(pokemon.getTipo())) {
                 pokemon.modificarVida((-1) * (pokemon.getVidaMax() * Constantes.disminucionPorClima));
@@ -49,11 +35,7 @@ public enum Clima {
         }
     }
 
-<<<<<<< HEAD
     public static Clima getClimaRandom() {
-=======
-    public static Clima getClima() {
->>>>>>> 4f5c1277241255de37e7d486195a94fc4e99bf77
         if (Random.probabilidad(Constantes.probabilidadConClima)) {
             int clima = Random.getRandom(0, 6);
             for (Clima c: Clima.values()) {
