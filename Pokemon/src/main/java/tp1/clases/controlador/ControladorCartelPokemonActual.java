18df26928e9a566daa177a293bfd3017467e496f
emiliaduzac <eduzac@fi.uba.ar>
2023-11-21 19:58:02 -0300

    visual casi lista

--- Pokemon/src/main/java/tp1/clases/controlador/ControladorCartelPokemonActual.java
+++ Pokemon/src/main/java/tp1/clases/controlador/ControladorCartelPokemonActual.java
@@ -3,46 +3,45 @@ package tp1.clases.controlador;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import tp1.clases.modelo.Jugador;
 import tp1.clases.modelo.Pokemon;
+import tp1.clases.modelo.Jugador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ControladorCartelPokemon implements Initializable {
-    @FXML
-    public HBox contenedorDatos;
+public class ControladorCartelPokemonActual implements Initializable {
+
     @FXML
     public Label labelNombre;
     @FXML
     public Label labelNivel;
     @FXML
-    public javafx.scene.control.ProgressBar barraVida;
-    @FXML
     public Label labelVida;
     @FXML
-    public ImageView imagenPokemonOpcion;
+    public ProgressBar barraVida;
+    @FXML
+    public ImageView imagenPokemonActual;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 
-    public void setDatosPokemon(Pokemon pokemon) {
+    public void setDatosPokemonActual(Pokemon pokemon) {
         this.labelNombre.setText(pokemon.getNombre());
         this.labelNivel.setText("Nvl." + pokemon.getNivel());
         this.labelVida.setText(pokemon.getVida() + "/" + pokemon.getVidaMax());
         this.barraVida.setProgress((double) pokemon.getVida() / pokemon.getVidaMax());
-        this.setImagenPokemonOpcion(pokemon.getNombre());
+        this.setImagenPokemonActual(pokemon.getNombre());
     }
 
-    private void setImagenPokemonOpcion(String pokemon){
+    private void setImagenPokemonActual(String pokemon){
         Image imagen = new Image("file:/home/emilia/Desktop/Algo3/TP/algo3-tp/Pokemon/src/main/resources/images/pokemon/" + pokemon + ".gif");
-        this.imagenPokemonOpcion.setImage(imagen);
+        this.imagenPokemonActual.setImage(imagen);
     }
+
 
 }
--- Pokemon/src/main/java/tp1/clases/controlador/ControladorMenuPokemon.java
+++ Pokemon/src/main/java/tp1/clases/controlador/ControladorMenuPokemon.java
@@ -1,31 +1,71 @@
 package tp1.clases.controlador;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import tp1.clases.modelo.Jugador;
 import tp1.clases.modelo.Pokemon;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
-
 public class ControladorMenuPokemon implements Initializable {
 
+    public VBox contenedorPokemonActual;
+    private  Jugador jugadorActual;
+    @FXML
     public VBox contenedorPokemon;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        // Puedes inicializar algo aquí si es necesario
     }
 
-    public void setPokemones(List<Pokemon> pokemones){
-        VBox vbox = new VBox();
-        for (Pokemon pokemon : pokemones){
-            Pane pane = new Pane();
+    public void setJugadorActual(Jugador jugadorActual){
+        this.jugadorActual = jugadorActual;
+    }
+
+    public void setPokemones(){
+        List<Pokemon> pokemones = jugadorActual.getListaPokemones();
+        for (Pokemon pokemon : pokemones) {
+            try {
+                if (pokemon == jugadorActual.getPokemonActual()){
+                    // Cargar el FXML para el Pokémon actual
+                    FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("cartelPokemonActual.fxml"));
+                    Pane cartelPokemonActual = loader.load();
+
+                    // Obtener el controlador del FXML para establecer la información del Pokémon
+                    ControladorCartelPokemonActual controladorCartelActual = loader.getController();
+                    controladorCartelActual.setDatosPokemonActual(pokemon);
+
+                    // Agregar el HBox al VBox
+                    contenedorPokemonActual.getChildren().add(cartelPokemonActual);
+
+                }else{
+                    // Cargar el FXML para cada Pokémon
+                    FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("cartelPokemon.fxml"));
+                    Pane cartelPokemon = loader.load();
+
+                    // Obtener el controlador del FXML para establecer la información del Pokémon
+                    ControladorCartelPokemon controladorCartel = loader.getController();
+                    controladorCartel.setDatosPokemon(pokemon);
+
+                    // Agregar el HBox al VBox
+                    contenedorPokemon.getChildren().add(cartelPokemon);
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    public ControladorMenuPokemon getControlador(){
+    public ControladorMenuPokemon getControlador() {
         return this;
     }
 }
